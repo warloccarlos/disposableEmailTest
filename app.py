bc3c93647347b324d3f4d5aee7ea4086bc11604d
Sandeep Khajuria <warloccarlos@gmail.com>
2023-02-18 01:59:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'Qwerty@123'
+app.config['MYSQL_PASSWORD'] = 'Password'
 app.config['MYSQL_DB'] = 'disposableemail'
 
 mysql = MySQL(app)
